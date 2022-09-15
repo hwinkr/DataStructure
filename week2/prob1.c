ddddbfc1c9a461f6ceac25cc23c3ea972420e484
woong <dnddl8280@naver.com>
2022-09-15 15:13:34 +0900

    [solved]

--- week2/prob1.c
+++ week2/prob1.c
@@ -4,8 +4,7 @@
 #define BUFFER_SIZE 20
 
 void check(char *str1, char *buffer_str);
-int main()
-{
+int main(){
     char word1[BUFFER_SIZE];
     char word2[BUFFER_SIZE];
 
@@ -18,19 +17,15 @@ int main()
     check(word1, newWord1);
     check(word2, newWord2);
 
-    if (strlen(newWord1) != strlen(newWord2))
-    {
+    if (strlen(newWord1) != strlen(newWord2)){
         printf("no");
     }
-    else
-    {
+    else{
         int cnt = 0;
-        for (int i = 0; i < strlen(word1); i++)
-        {
-            for (int j = 0; j < strlen(word2); j++)
-            {
-                if (word1[i] == word2[j])
-                {
+        int len = strlen(word1);
+        for (int i = 0; i < len; i++){
+            for (int j = 0; j < len; j++){
+                if (word1[i] == word2[j]){
                     cnt += 1;
                     break;
                 }
@@ -44,27 +39,21 @@ int main()
     return 0;
 }
 
-void check(char *str1, char *buffer_str)
-{
-
+void check(char *str1, char *buffer_str){
     buffer_str[0] = str1[0];
     bool isSame = false;
 
-    for (int i = 1; i < strlen(str1); i++)
-    {
-
-        for (int j = 0; j < strlen(buffer_str); j++)
-        {
-            if (str1[i] == buffer_str[j])
-            {
+    for (int i = 1; i < strlen(str1); i++){
+        for (int j = 0; j < strlen(buffer_str); j++){
+            if (str1[i] == buffer_str[j]){
                 isSame = true;
                 break;
             }
         }
-        if (isSame)
+        if (isSame){
             isSame = false;
-        else
-        {
+        }
+        else{
             buffer_str[strlen(buffer_str)] = str1[i];
             isSame = false;
         }
