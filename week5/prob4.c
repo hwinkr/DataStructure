10a3734ffeebd8e56523026f8ba45e05f4222663
woong <dnddl8280@naver.com>
2022-10-06 17:27:30 +0900

    [solved]

---
+++ week5/prob4.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#define MAX 100
+
+int main(){
+    FILE * fp = fopen("sample.html", "r");
+    FILE *result = fopen("sample.txt", "w");
+
+    char buffer; 
+    bool istag = false;
+
+    while(fscanf(fp, "%c", &buffer) != EOF){
+        if(buffer == '<'){
+            istag = true;
+            continue;
+        }else if(buffer == '>'){
+            istag = false;
+            continue;
+        }
+
+        if(istag) continue;
+        else fprintf(result, "%c", buffer);
+    }
+    fclose(fp);
+    
+    return 0;
+}
