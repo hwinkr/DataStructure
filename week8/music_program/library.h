eadf5486749b7c3fc22b908244ce1e095413705b
woong <dnddl8280@naver.com>
2022-11-01 15:50:04 +0900

    [Header File]

--- week8/music_program/library.h
+++ week8/music_program/library.h
@@ -9,33 +9,31 @@ typedef struct song Song;
 typedef struct snode SNode;
 typedef struct artist Artist;
 
-struct song
-{
+struct song{
     Artist *artist;
     char *title;
     char *path;
     int index;
 };
 
-struct snode
-{
+struct snode{
     SNode *prev, *next;
     Song *song;
 };
 
-struct artist
-{
+struct artist{
     char *name;
     // * artist 의 이름을 사전식 순으로 정렬
     Artist *next;
     SNode *head, *tail;
 };
-
+// ! header 파일에서 프로토타입을 선언하는 함수는 main.cpp 소스 파일에서 사용하는 함수들
 void initiallize();
 void add_song(char *artist, char *title, char *path);
 void status();
 void load(FILE *fp);
 void search_song(char *artist);
 void search_song(char *artist, char *title);
+void play(int index);
 
 #endif
